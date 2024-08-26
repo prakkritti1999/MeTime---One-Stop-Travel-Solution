8e74574829ba823525d5b86e60aa55be53f42a08
prakkritti1999 <prakriti.guptta@gmail.com>
2024-08-26 11:56:06 +0530

    specify version of Java

--- src/main/java/com/metime/dao/User.java
+++ src/main/java/com/metime/dao/User.java
@@ -28,6 +28,25 @@ public class User {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 	
 	
 	
